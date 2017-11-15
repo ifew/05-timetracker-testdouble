a33a8098f9e836c076e7c885a25532eb90a9de54
iFew <chitpong@gmail.com>
2017-11-15 14:53:33 +0700

    refactor

--- TimeTracker.cs
+++ TimeTracker.cs
@@ -4,17 +4,19 @@ namespace _05_timetracker_testdouble
 {
     internal class TimeTracker
     {
+        
+        TimeSpan TimeNow = DateTime.Now.TimeOfDay;
+        
         public TimeTracker()
         {
         }
 
         internal bool isOpen()
         {
-            TimeSpan TimeNow = DateTime.Now.TimeOfDay;
             
             TimeSpan TimeOpenStart = new TimeSpan(8,0,0);
             TimeSpan TimeOpenEnd = new TimeSpan(17,0,0);
-            
+
             int CheckOpenStart = TimeSpan.Compare(TimeNow, TimeOpenStart);
             int CheckOpenEnd = TimeSpan.Compare(TimeNow, TimeOpenEnd);
             
