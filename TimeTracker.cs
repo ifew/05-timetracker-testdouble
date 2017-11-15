f098a227a8678f2e44babbd4a165e3a0de426f4e
iFew <chitpong@gmail.com>
2017-11-15 13:55:39 +0700

    implement code for test pass

---
+++ TimeTracker.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace _05_timetracker_testdouble
+{
+    internal class TimeTracker
+    {
+        public TimeTracker()
+        {
+        }
+
+        internal bool isOpen(string v)
+        {
+            return true;
+        }
+    }
+}
